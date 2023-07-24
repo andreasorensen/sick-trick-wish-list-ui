10df78cf73ac00f3a89346446cf7d9007714e901
Andrea Sorensen <dreasor2987@icloud.com>
2023-07-24 08:58:16 -0700

    feat/moved useEffect to App.js as well as state

---
+++ src/components/TrickCard/TrickCard.js
@@ -0,0 +1,20 @@
+import './TrickCard.css';
+import React, { useEffect, useState } from "react";
+
+const TrickCard = ({ tricks }) => {
+
+  return (
+    <div className='cards-container'>
+      {tricks.map((trick) => (
+        <div className='trick-card' key={trick.id}>
+          <p className='trick-title'>{trick.name}</p>
+          <p>Stance: {trick.stance}</p>
+          <p>Obstacle: {trick.obstacle}</p>
+          <a href={trick.tutorial} className='trick-link'>Tutorial Link</a>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default TrickCard;
