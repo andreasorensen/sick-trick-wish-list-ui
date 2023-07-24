8b2e5b23943a340ef8fd0dd614f929a9ee11e0aa
Andrea Sorensen <dreasor2987@icloud.com>
2023-07-24 08:59:26 -0700

    feat/App component updated with handle function for on click

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,33 @@
 import './App.css';
-import React from 'react';
-import TrickCard from './TrickCard/TrickCard';
+import TrickCard from '../TrickCard/TrickCard';
+import TrickForm from '../TrickForm/TrickForm';
+import React, { useEffect, useState } from 'react';
 
 
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Sick Trick Wish List</h1>
-      <TrickCard />
-      <div></div>
-    </div>
-  );
+
+  const [tricks, setTricks] = useState([]);
+
+  const handleNewTrick = (newTrick) => {
+    setTricks([...tricks, newTrick]);
+  };
+
+  useEffect(() => {
+    fetch("http://localhost:3001/api/v1/tricks")
+      .then((response) => response.json())
+      .then((data) => setTricks(data))
+      .catch((err) => console.error("Error: ", err));
+  }, []);
+
+    return (
+      <div className="App">
+        <h1>Sick Trick Wish List</h1>
+        <TrickForm onNewTrick={handleNewTrick} />
+        <TrickCard tricks={tricks} />
+        <div></div>
+      </div>
+    );
 }
 
 export default App; 
