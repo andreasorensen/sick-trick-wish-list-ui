1917288cf4cb59f72115bbd72d0be0eeececbbe9
Andrea Sorensen <dreasor2987@icloud.com>
2023-07-24 08:58:52 -0700

    feat/TrickForm completed without styling

---
+++ src/components/TrickForm/TrickForm.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+
+const TrickForm = ({ onNewTrick }) => {
+  const [stance, setStance] = useState('');
+  const [name, setName] = useState('');
+  const [obstacle, setObstacle] = useState('');
+  const [tutorial, setTutorial] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onNewTrick({ stance, name, obstacle, tutorial });
+    setStance('');
+    setName('');
+    setObstacle('');
+    setTutorial('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Stance:
+        <select value={stance} onChange={(e) => setStance(e.target.value)}>
+          <option value="">Select a stance</option>
+          <option value="Regular">Regular</option>
+          <option value="Switch">Switch</option>
+        </select>
+      </label>
+
+      <label>
+        Name:
+        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+      </label>
+
+      <label>
+        Obstacle:
+        <select value={obstacle} onChange={(e) => setObstacle(e.target.value)}>
+          <option value="">Select an obstacle</option>
+          <option value="Flatground">Flatground</option>
+          <option value="Ledge">Ledge</option>
+          <option value="Rail">Rail</option>
+          <option value="Stairs">Stairs</option>
+          <option value="Pool">Pool</option>
+        </select>
+      </label>
+
+      <label>
+        Tutorial:
+        <input type="text" value={tutorial} onChange={(e) => setTutorial(e.target.value)} />
+      </label>
+
+      <button type="submit">SEND IT</button>
+    </form>
+  );
+};
+
+export default TrickForm;
